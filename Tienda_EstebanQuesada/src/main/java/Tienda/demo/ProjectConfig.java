982d1097f521eee83bc95f9cf3f396d3267aaad8
EstebanQuesada <equesada00666@ufide.ac.cr>
2024-02-14 20:54:15 -0600

    semana 5

--- Tienda_EstebanQuesada/src/main/java/Tienda/demo/ProjectConfig.java
+++ Tienda_EstebanQuesada/src/main/java/Tienda/demo/ProjectConfig.java
@@ -2,18 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package practicaprogramada;
+package Tienda.demo;
 
 /**
  *
  * @author EsteP
  */
-public class Ejercicio5 {
-    public void tablas(int i, int n){
-        if(i<=10){
-            System.out.print(i+"*"+ n + "=" + i*n + "\n" );
-            tablas(i+1, n);
-        }
-    }
-}
+public class ProjectConfig {
     
+}
