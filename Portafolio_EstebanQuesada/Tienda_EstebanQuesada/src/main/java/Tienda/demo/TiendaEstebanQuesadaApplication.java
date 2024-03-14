2449f850c863f6bac665b37f6b4d3e4722f4c3dc
EstebanQuesada <equesada00666@ufide.ac.cr>
2024-03-14 00:32:05 -0600

    semana 8 (La pasada era la semana 7)

---
+++ Portafolio_EstebanQuesada/Tienda_EstebanQuesada/src/main/java/Tienda/demo/TiendaEstebanQuesadaApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEstebanQuesadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEstebanQuesadaApplication.class, args);
+	}
+
+}
